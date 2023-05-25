96554906edaf05db19cc848957fb58b15bcd2b8b
Sanskar Modi <modisanskar5@gmail.com>
2023-05-26 00:07:28 +0530

    Tech Section Added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <>
+        <div className="flex flex-col flex-wrap justify-center gap-5">
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+        <p className="text-center">{technology.name}</p>
+        </div>
+        </>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper( Tech, '');
