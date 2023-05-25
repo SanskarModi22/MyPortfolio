22cd5dac3f36c1e8d26e68b8037a622a6941811e
Sanskar Modi <modisanskar5@gmail.com>
2023-05-25 18:16:40 +0530

    Hero Portion Added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,52 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl max-auto flex flex-row items-start gap-5`}
+      >
+        <div className={`flex flex-col justify-center items-center mt-5`}>
+          <div className={`w-5 h-5 rounded-full bg-[#915eff]`} />
+          <div className={`w-1 sm:h-80 h-40 violet-gradient`} />
+        </div>
 
-export default Hero
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I&apos;m <span className={`text-[#915eff]`}>Sanskar</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals ,user <br className="sm:block hidden" />{" "}
+            interfaces and web applications
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-40 w-full flex justify-center items-center">
+        {/* A link that navigates to the element with the ID "about" */}
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            {/* An animated element */}
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5, // Animation duration of 1.5 seconds
+                repeat: Infinity, // Animation repeats indefinitely
+                repeatType: "loop", // Animation restarts from the beginning after each cycle
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              // The Ball Element inside the animation
+            ></motion.dev>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
